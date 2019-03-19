77574dae0ffef75d74d56117b7c199234b83d100
Sebastian Nicolae Cocîrlă <sebastian.cocirla@tecss.com>
2019-03-19 14:40:24 +0200

    simplified the register

--- ProiectColectiv/Controllers/RegisterController.cs
+++ ProiectColectiv/Controllers/RegisterController.cs
@@ -25,37 +25,12 @@ namespace ProiectColectiv.Controllers
                 {
                     throw new System.FormatException();
                 }
-                return RedirectToAction("Login", "Login");
+                return View("~/Views/Home/RegisterSuccess.cshtml");
             }
             catch(System.FormatException)
             {
-                return RedirectToAction("RegisterError", "Register");
-            }
-        }
-        [HttpGet]
-        public ActionResult RegisterError()
-        {
-            var user = new User();
-            return View("~/Views/Home/RegisterError.cshtml", user);
-        }
-        [HttpPost]
-        public ActionResult RegisterError(User user)
-        {
-            try
-            {
-                MailAddress m = new MailAddress(user.Email);
-                Regex regex = new Regex(@"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,15})$");
-                Regex regex2 = new Regex(@"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
-                if (!regex.IsMatch(user.Password) || !regex2.IsMatch(user.Name))
-                {
-                    throw new System.FormatException();
-                }
-                return RedirectToAction("Login", "Login");
-            }
-            catch (System.FormatException)
-            {
-                return RedirectToAction("RegisterError", "Register");
-            }
-        }
+                return View("~/Views/Home/RegisterError.cshtml");
+            }            
+        }       
     }
 }
